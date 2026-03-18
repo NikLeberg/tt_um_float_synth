7f3cdb61b7f3247ab3362af832006c6afcca925f
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-03-18 21:11:57 +0000

    src: generate Verilog from VHDL

---
+++ src/gen/gen.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+set -e
+set -x # local command echo
+
+# Restart this script inside docker container with GHDL tooling.
+if [ -z "$IN_DOCKER" ]; then
+    docker run --rm -it \
+        --env IN_DOCKER=1 \
+        --volume $(realpath ..):/work \
+        --workdir /work/gen \
+        --entrypoint bash \
+        -u ubuntu \
+        ghcr.io/nikleberg/formal:2026-03-09 \
+        -c "./gen.sh"
+
+    exit 0
+fi
+
+# Analyze VHDL sources.
+ghdl -a --std=08 ../vhdl/float8_pkg.vhd
+ghdl -a --std=08 ../vhdl/fadd8.vhd
+
+# Synthesize to optimized but highlevel Verilog.
+yosys -m ghdl -s synth.ys
