012af58b0d0d1a579d2092f9b5875829d600d553
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2026-03-19 21:59:13 +0000

    src: pre-synth with retiming

--- src/gen/synth.tcl
+++ src/gen/synth.tcl
@@ -5,10 +5,18 @@ yosys -import
 # Load VHDL design into Yosys.
 ghdl --std=08 --no-formal -gN_STAGES=$::env(N_STAGES) $::env(TOP)
 
-# Do some minimal optimization.
-opt -full -purge
-wreduce
-opt -full -purge
+# Do a generic but full synthesis.
+synth -flatten
 
-# Save to pre processed Verilog.
+# Legalize FFs for ABC.
+dfflegalize -cell \$_DFF_P_ 0
+
+# Invoke ABC with exposed FFs and run customized retiming script.
+abc -dff -script retime.abc
+opt -full -purge
+stat
+
+write_json dump.json
+
+# Save to synthesized Verilog netlist.
 write_verilog $::env(TOP).v
